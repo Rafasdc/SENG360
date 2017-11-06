ec9d24d407d74c6367ce0cd85c525f0526378881
Rafael Solorzano <rafa@solorzanoc.com>
2017-11-06 11:26:22 -0800

    First encryption

--- SENG360Asn3/src/ClientOperation.java
+++ SENG360Asn3/src/ClientOperation.java
@@ -2,21 +2,51 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
+import javax.crypto.*;
 import javax.swing.JOptionPane;
 
 public class ClientOperation {
 	private static RMIInterface look_up;
 
-	public static void main(String[] args) throws MalformedURLException, RemoteException, NotBoundException {
+	public static void main(String[] args) throws MalformedURLException, RemoteException, NotBoundException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
 		
+		KeyGenerator keygen = KeyGenerator.getInstance("AES");
+	    SecretKey aesKey = keygen.generateKey();
+	    
+	    Cipher aesCipher;
+	    
+	    // Create the cipher
+	    aesCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+	    
+	    // Initialize the cipher for encryption
+	    aesCipher.init(Cipher.ENCRYPT_MODE, aesKey);
+
+	    // Our cleartext
+	    byte[] cleartext = "This is just an example".getBytes();
+
+	    // Encrypt the cleartext
+	    byte[] ciphertext = aesCipher.doFinal(cleartext);
+
+	    // Initialize the same cipher for decryption
+	    aesCipher.init(Cipher.DECRYPT_MODE, aesKey);
+
+	    // Decrypt the ciphertext
+	    
+	    
+	    byte[] cleartext1 = aesCipher.doFinal(ciphertext);
+	    
+	    String firstText = new String (cleartext);
+	    
+	    System.out.println(firstText);
 		
 		look_up = (RMIInterface) Naming.lookup("//localhost/MyServer");
-		while(true){
 		String txt = JOptionPane.showInputDialog("What is your name?");
 			
 		String response = look_up.helloTo(txt);
-		JOptionPane.showMessageDialog(null, response);
-		}
+		System.out.println(response);
+		//JOptionPane.showMessageDialog(null, response);
 	}
 }
